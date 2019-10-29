e94fab13bab614e62c9cdd9078817427c3cebf4a
George Wyscaver <gwyscaver3@gmail.com>
2019-10-28 19:19:16 -0700

    Updates

---
+++ precache-manifest.f21113a34a3b933e88d32f3d4f26b635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3829f36f7e3df7ace94de25e107d2f40",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "2535bd91d36a35c15acc",
+    "url": "/google-books-search/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f28aeb2c568823c952e0",
+    "url": "/google-books-search/static/js/2.e5500bba.chunk.js"
+  },
+  {
+    "revision": "2535bd91d36a35c15acc",
+    "url": "/google-books-search/static/js/main.7dcd51d7.chunk.js"
+  },
+  {
+    "revision": "9cec58e7c846d032405a",
+    "url": "/google-books-search/static/js/runtime-main.4c7b2f57.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/google-books-search/static/media/logo.25bf045c.svg"
+  }
+]);
